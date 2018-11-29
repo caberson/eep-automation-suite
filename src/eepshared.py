6528ade4805a02b596d44259ea3293171239801c
Caber Chu <caber@kargo.com>
2018-11-29 00:39:21 -0800

    Use haar cascade files from opencv site package

--- src/eepshared.py
+++ src/eepshared.py
@@ -10,7 +10,6 @@ import re
 from datetime import datetime
 
 import eeputil
-import dir
 
 USER_DIR = os.path.expanduser('~')
 EEP_DOC_DIR = os.getenv(
@@ -20,21 +19,44 @@ EEP_DOC_DIR = os.getenv(
 current_year = datetime.now().year
 current_month = datetime.now().month
 SUGGESTED_FILE_DESTINATION_FOLDER_NAME = ('/' + str(current_year) +
-	('s' if current_month <= 6 else 'f'))
+    ('s' if current_month <= 6 else 'f'))
 SUGGESTED_RAW_EXCEL_FILE_BASE_NA = (str(current_year) +
-	('s' if current_month <= 6 else 'f') + '_eep')
+    ('s' if current_month <= 6 else 'f') + '_eep')
 DESTINATION_DIR = (
-	EEP_DOC_DIR +
-	SUGGESTED_FILE_DESTINATION_FOLDER_NAME + '/'
+    EEP_DOC_DIR +
+    SUGGESTED_FILE_DESTINATION_FOLDER_NAME + '/'
 )
 
 INSPECTION_DOCUMENTS_DESTINATION_DIR = DESTINATION_DIR + 'documents_inspection/'
 
 STUDENT_NAME_LABELS_DIR = DESTINATION_DIR + 'student_name_labels/'
 
-STUDENT_PHOTOS_ORIGINAL_DIR = DESTINATION_DIR + dir.PHOTOS_ORIGINAL_FOLDER_NAME
-STUDENT_PHOTOS_CROPPED_DIR = DESTINATION_DIR + dir.PHOTOS_CROPPED_FOLDER_NAME
+PHOTOS_ORIGINAL_FOLDER_NAME = 'eep_photos_original'
+PHOTOS_CROPPED_FOLDER_NAME = 'eep_photos_cropped'
+
+STUDENT_PHOTOS_ORIGINAL_DIR = DESTINATION_DIR + PHOTOS_ORIGINAL_FOLDER_NAME
+STUDENT_PHOTOS_CROPPED_DIR = DESTINATION_DIR + PHOTOS_CROPPED_FOLDER_NAME
+
+DIR_APP = os.getcwd()
+DIR_DATA = os.path.join(DIR_APP, 'data')
+DIR_EEP_PHOTOS_ORIGINAL_DEFAULT = os.path.join(DIR_DATA, PHOTOS_ORIGINAL_FOLDER_NAME)
+DIR_EEP_PHOTOS_CROPPED_DEFAULT = os.path.join(DIR_DATA, PHOTOS_CROPPED_FOLDER_NAME)
+
+DIR_TEMPLATES = os.path.join(DIR_APP, 'templates')
+DIR_ASSETS = os.path.join(DIR_APP, 'assets')
+DIR_OUTPUT = os.path.join(DIR_APP, 'output')
+
+def get_config(config_file=None):
+    import ConfigParser
+    if not config_file:
+        CONFIG_NAME = 'eep.cfg'
+        config_file = os.path.join(os.getcwd(), 'etc', CONFIG_NAME)
+
+    config = ConfigParser.SafeConfigParser()
+    config.read(config_file)
+    return config
+
 
 OUTPUT_ENCODING = 'utf-8'
 if sys.platform == 'win32':
-	OUTPUT_ENCODING = 'big5'
+    OUTPUT_ENCODING = 'big5'
